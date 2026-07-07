e918ca7ba26dcfb2fced07f4bd56ad6f6b888e6e
naluwa <tharushadilshan4u@gmail.com>
2026-07-07 12:36:03 +0530

    create frontend constants module

---
+++ frontend/src/config/constants.js
@@ -0,0 +1,20 @@
+export const PAYMENT_STATUS = {
+  PENDING: "PENDING",
+  PAID: "PAID",
+};
+
+export const VIEWS = {
+  CUSTOMER: "customer",
+  ADMIN: "admin",
+  LOGIN: "login", // admin login
+  CUSTOMER_LOGIN: "customer_login",
+};
+
+
+
+export const TOAST_TYPES = {
+  SUCCESS: "success",
+  ERROR: "error",
+  INFO: "info",
+};
+
